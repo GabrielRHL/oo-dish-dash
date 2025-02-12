aef32ab64d4b62d01bdf4b746679e7a7501dec56
Gabriel <gabriell-rocha@hotmail.com>
2025-02-12 13:47:19 -0300

    criando return do name

--- models/menu/drink.py
+++ models/menu/drink.py
@@ -4,3 +4,6 @@ class Drink(Menu):
     def __init__(self, name, price, description):
         super().__init__(name, price)
         self._description =  description
+
+    def __str__(self):
+        return self._name
