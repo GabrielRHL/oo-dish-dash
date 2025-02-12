0c54ee0b3d4ad2cb1ee4072a322e6754dafe88e5
Gabriel <gabriell-rocha@hotmail.com>
2025-02-12 13:46:02 -0300

    fazendo import e testando dish.py e drink.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from models.restaurant import Restaurant
+from models.menu.dish import Dish
+from models.menu.drink import Drink
 
 restaurant_granu = Restaurant('Granulado', 'Doceria')
 restaurant_coco_bambu = Restaurant('Coco Bambu', 'Frutos do mar')
@@ -6,17 +8,22 @@ restaurant_il_piacere = Restaurant('Il Piacere', 'Pizzaria')
 
 Restaurant.change_state(restaurant_granu)
 
-restaurant_granu.receive_rate('Gabriel', 3)
-restaurant_granu.receive_rate('Grazielle', 3)
+# restaurant_granu.receive_rate('Gabriel', 3)
+# restaurant_granu.receive_rate('Grazielle', 3)
 
-restaurant_coco_bambu.receive_rate('Gabriel', 5)
-restaurant_coco_bambu.receive_rate('Grazielle', 4)
+# restaurant_coco_bambu.receive_rate('Gabriel', 5)
+# restaurant_coco_bambu.receive_rate('Grazielle', 4)
 
-restaurant_il_piacere.receive_rate('Gabriel', 5)
-restaurant_il_piacere.receive_rate('Grazielle', 5)
+# restaurant_il_piacere.receive_rate('Gabriel', 5)
+# restaurant_il_piacere.receive_rate('Grazielle', 5)
+
+juice_drink = Drink('Suco de laranja', 5, '500ml')
+bread_dish = Dish('Pão de queijo', 4, 'Mini pão de queijo')
 
 def main():
-    Restaurant.display_restaurants()
+    print(bread_dish)
+    print(juice_drink)
+    # Restaurant.display_restaurants()
 
 if __name__ == '__main__':
     main()
