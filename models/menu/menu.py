4534dfbac253240ed41702b3e52e4c19a6b54387
Gabriel <gabriell-rocha@hotmail.com>
2025-02-12 16:38:06 -0300

    importando abstractmethod e criando o metodo apply_discount

--- models/menu/menu.py
+++ models/menu/menu.py
@@ -1,4 +1,10 @@
-class Menu:
+from abc import ABC, abstractmethod
+
+class Menu(ABC):
     def __init__(self, name, price):
         self._name = name
         self._price = price
+
+    @abstractmethod
+    def apply_discount(self):
+        pass
